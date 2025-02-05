1d6d151db9ced80348fbaa38ba238afe0af45904
Besnik Berbatovci <besib9074@gmail.com>
2025-02-05 23:18:36 +0100

    make dinamyc share post

--- route/share.route.js
+++ route/share.route.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const SharePostModel = require("../model/sharePost.model");
+router.get("/:id", async (req, res) => {
+  const postId = req.params.id;
+  const postDetails = await SharePostModel.getShareProduct(postId);
 
-router.get("/share/:id", (req, res) => {
   const post = {
     id: postId,
-    title: "Raiffeisen Plus Digital Identity",
-    description:
-      "Raiffeisen Plus, the innovative digital banking platform by Raiffeisen Bank Kosovo",
-    image:
-      "http://178.128.197.35/uploads/portfolio/content/1736255100877-01.png",
+    title: postDetails?.title,
+    description: postDetails?.description,
+    image: `http://178.128.197.35/${postDetails?.photo}`,
+    url: `http://178.128.197.35/work/${postId}`,
   };
 
   res.send(`
-    <!DOCTYPE html>
+ <!DOCTYPE html>
     <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta property="og:title" content="${post.title}" />
         <meta property="og:description" content="${post.description}" />
         <meta property="og:image" content="${post.image}" />
-        <meta property="og:url" content="http://178.128.197.35/api/share" />
+        <meta property="og:url" content="${post.url}" />
         <meta name="twitter:card" content="summary_large_image">
         <title>${post.title}</title>
     </head>
